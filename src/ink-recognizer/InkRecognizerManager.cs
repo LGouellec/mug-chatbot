cb86a9ff7d6d2567f8c953eec10e020cf27fbdb7
LGouellec <sylvain.le-gouellec_ext@michelin.com>
2019-12-04 22:52:14 +0100

    DONE Pres

---
+++ src/ink-recognizer/InkRecognizerManager.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ink_recognizer
+{
+    public class InkRecognizerRequest
+    {
+        public class Strokes
+        {
+            public int id { get; set; }
+            public int points { get; set; }
+        }
+
+        public string language { get; set; }
+        public int version { get; set; }
+        public Strokes[] strokes { get; set; } = new Strokes[] { };
+    }
+
+    public class InkRecognizerManager
+    {
+        private readonly string endpoint = "https://ink-mug.cognitiveservices.azure.com/";
+        private readonly string apiKey = "e4af79f1228b43a3b67461906f5a9129";
+        private readonly string inkRecognitionUrl = "/inkrecognizer/v1.0-preview/recognize";
+
+
+        public async Task<string> Request(InkRecognizerRequest request)
+        {
+            RestClient client = new RestClient(endpoint);
+            RestRequest restRequest = new RestRequest(inkRecognitionUrl, Method.PUT);
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddJsonBody(request);
+
+            var respone = await client.ExecuteTaskAsync<string>(restRequest);
+            return respone.Data;
+        }
+    }
+}
---
+++ src/ink-recognizer/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ink_recognizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Point currentPoint = new Point();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(this).X;
+                line.Y2 = e.GetPosition(this).Y;
+
+                currentPoint = e.GetPosition(this);
+                this.canvas.Children.Add(line);
+            }
+        }
+
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(this);
+        }
+
+        private void canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.canvas.Children.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                theInkCanvas.Strokes.Save(ms);
+                var myInkCollector = new InkCollector();
+                var ink = new Ink();
+                ink.Load(ms.ToArray());
+
+                using (RecognizerContext context = new RecognizerContext())
+                {
+                    if (ink.Strokes.Count > 0)
+                    {
+                        context.Strokes = ink.Strokes;
+                        RecognitionStatus status;
+
+                        var result = context.Recognize(out status);
+
+                        if (status == RecognitionStatus.NoError)
+                            textBox1.Text = result.TopString;
+                        else
+                            MessageBox.Show("Recognition failed");
+
+                    }
+                    else
+                        MessageBox.Show("No stroke detected");
+                }
+            }
+
+            InkRecognizerManager manager = new InkRecognizerManager();
+            string result = manager.Request(GetRequest()).GetAwaiter().GetResult();
+        }
+
+        private InkRecognizerRequest GetRequest()
+        {
+            InkRecognizerRequest r = new InkRecognizerRequest();
+            r.language = "fr-FR";
+            r.version = 1;
+            int id = 1;
+
+            foreach(var c in this.canvas.Children)
+            {
+                if(c is Line)
+                {
+                    Line line = (Line)c;
+                    
+                }
+            }
+
+
+
+
+            return r;
+        }
+    }
+}
