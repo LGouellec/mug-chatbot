e81a05570a0ca9e54e8abb8967b98f249c45b561
LGouellec <sylvain.le-gouellec_ext@michelin.com>
2019-12-05 10:26:28 +0100

    Ink Recognizer

--- src/ink-recognizer/InkRecognizerManager.cs
+++ src/ink-recognizer/InkRecognizerManager.cs
@@ -2,11 +2,13 @@
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Ink;
 
 namespace ink_recognizer
 {
@@ -15,12 +17,23 @@ namespace ink_recognizer
         public class Strokes
         {
             public int id { get; set; }
-            public int points { get; set; }
+            public string points { get; set; }
         }
 
         public string language { get; set; }
         public int version { get; set; }
-        public Strokes[] strokes { get; set; } = new Strokes[] { };
+        public List<Strokes> strokes { get; set; } = new List<Strokes> { };
+
+        internal void AddStroke(int v, Stroke c)
+        {
+            string points = "";
+            foreach(var point in c.StylusPoints)
+            {
+                points += $"{point.X},{point.Y},";
+            }
+            points = points.Remove(points.Length - 1, 1);
+            strokes.Add(new Strokes { id = v, points = points });
+        }
     }
 
     public class InkRecognizerManager
@@ -30,15 +43,34 @@ namespace ink_recognizer
         private readonly string inkRecognitionUrl = "/inkrecognizer/v1.0-preview/recognize";
 
 
-        public async Task<string> Request(InkRecognizerRequest request)
+        public string Request(InkRecognizerRequest request)
         {
             RestClient client = new RestClient(endpoint);
             RestRequest restRequest = new RestRequest(inkRecognitionUrl, Method.PUT);
             restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddHeader("Ocp-Apim-Subscription-Key", apiKey);
             restRequest.AddJsonBody(request);
 
-            var respone = await client.ExecuteTaskAsync<string>(restRequest);
-            return respone.Data;
+            var response = client.Execute(restRequest);
+            if (response.StatusCode == HttpStatusCode.OK)
+                return AnalyseResult(JsonConvert.DeserializeObject<RootObject>(response.Content));
+            else
+                return $"Error : {response.StatusCode} - {response.Content}";
+        }
+
+        private string AnalyseResult(RootObject response)
+        {
+            var units = response.recognitionUnits.FirstOrDefault(r => r.category.Equals("line"));
+            if (units != null)
+                return units.recognizedText;
+            else
+            {
+                var inkWordUnits = response.recognitionUnits.Where(r => r.category.Equals("inkWord")).OrderBy(r => r.id);
+                string result = "";
+                foreach (var i in inkWordUnits)
+                    result += $"{i.recognizedText} ";
+                return result;
+            }
         }
     }
 }
--- src/ink-recognizer/MainWindow.xaml.cs
+++ src/ink-recognizer/MainWindow.xaml.cs
@@ -26,67 +26,16 @@ namespace ink_recognizer
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-        private void canvas_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Line line = new Line();
 
-                line.Stroke = SystemColors.WindowFrameBrush;
-                line.X1 = currentPoint.X;
-                line.Y1 = currentPoint.Y;
-                line.X2 = e.GetPosition(this).X;
-                line.Y2 = e.GetPosition(this).Y;
-
-                currentPoint = e.GetPosition(this);
-                this.canvas.Children.Add(line);
-            }
-        }
-
-        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ButtonState == MouseButtonState.Pressed)
-                currentPoint = e.GetPosition(this);
-        }
-
-        private void canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.canvas.Children.Clear();
-        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                theInkCanvas.Strokes.Save(ms);
-                var myInkCollector = new InkCollector();
-                var ink = new Ink();
-                ink.Load(ms.ToArray());
-
-                using (RecognizerContext context = new RecognizerContext())
-                {
-                    if (ink.Strokes.Count > 0)
-                    {
-                        context.Strokes = ink.Strokes;
-                        RecognitionStatus status;
-
-                        var result = context.Recognize(out status);
-
-                        if (status == RecognitionStatus.NoError)
-                            textBox1.Text = result.TopString;
-                        else
-                            MessageBox.Show("Recognition failed");
-
-                    }
-                    else
-                        MessageBox.Show("No stroke detected");
-                }
-            }
-
             InkRecognizerManager manager = new InkRecognizerManager();
-            string result = manager.Request(GetRequest()).GetAwaiter().GetResult();
+            string result = manager.Request(GetRequest());
+            this.result.Text = result;
         }
 
         private InkRecognizerRequest GetRequest()
@@ -96,19 +45,17 @@ namespace ink_recognizer
             r.version = 1;
             int id = 1;
 
-            foreach(var c in this.canvas.Children)
-            {
-                if(c is Line)
-                {
-                    Line line = (Line)c;
-                    
-                }
-            }
 
-
-
+            foreach (var c in this.canvas.Strokes)
+                r.AddStroke(id++, c);
 
             return r;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            canvas.Strokes.Clear();
+            this.result.Text = "";
         }
     }
 }
